106114b1f9be8f6b1cb6f44558550a11924b0753
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-08-07 22:59:32 +0200

    done wrong

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-const modeToggle = document.getElementById("mode-toggle");
-const body = document.body;
+function clearScreen() {
+  resultElement.value = "";
+  currentExpression = "";
+}
 
-modeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-  const isDarkMode = body.classList.contains("dark-mode");
-  //   we set it using local storage
-  localStorage.setItem("dark-mode", isDarkMode);
+document.querySelector(".buttons").addEventListener("click", (event) => {
+  if (event.target.value === "Del") {
+    resultElement.value = resultElement.value.slice(0, -1);
+    currentExpression = currentExpression.slice(0, -1);
+  }
 });
-
-// we test local storage
-
-const savedMode = localStorage.getItem("dark-mode");
-if (savedMode === "true") {
-  body.classList.add("dark-mode");
-}
-
-function display(val) {
-  var upperDiv = document.getElementById("upper");
-  upperDiv.textContent = upperDiv.textContent + val;
-
-  return val;
-}
