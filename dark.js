07a426b6b7647509b22a731fcea6a16ee460646d
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-08-13 00:25:57 +0200

    created a separated for themes

--- dark.js
+++ dark.js
@@ -4,13 +4,20 @@ const body = document.body;
 modeToggle.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
   const isDarkMode = body.classList.contains("dark-mode");
-  //   we set it using local storage
   localStorage.setItem("dark-mode", isDarkMode);
+  updateModeToggleText(isDarkMode);
 });
 
-// we test local storage
+function updateModeToggleText(isDarkMode) {
+  modeToggle.innerHTML = isDarkMode ? "White-Theme" : "Dark-Theme";
+}
+
+// Retrieve and apply the saved theme mode from local storage
 
 const savedMode = localStorage.getItem("dark-mode");
 if (savedMode === "true") {
   body.classList.add("dark-mode");
+  updateModeToggleText(true);
+} else {
+  updateModeToggleText(false);
 }
