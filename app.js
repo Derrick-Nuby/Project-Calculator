55de98df91dbcc8e6e4e295fde91e4596b90d1ad
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-08-14 17:26:30 +0200

    all fns minus calculate

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-const one = document.getElementById("one");
 const operations = document.getElementById("operations");
 const answer = document.getElementById("answer");
-const operand1 = [];
+let operand1 = [];
+let operand2 = [];
 let result;
+let fNumber, sNumber;
 
 function allocate(number) {
-  display(number);
   operands(number);
+  display(number);
+  // display(number);
+  // operand1.push(number);
+  // fNumber = Number(operand1.join(""));
+  // console.log(fNumber);
+  // if (typeof number === "number") {
+  //   operand1.push(number);
+  //   fNumber = Number(operand1.join(""));
+  //   console.log(fNumber);
+  // } else if (number === "-") {
+  //   operand2.push(number);
+  //   sNumber = Number(operand2.join(""));
+  // } else if (number === "+") {
+  //   operand2.push(number);
+  //   sNumber = Number(operand2.join(""));
+  // } else if (number === "X") {
+  //   operand2.push(number);
+  //   sNumber = Number(operand2.join(""));
+  // } else if (number === "*") {
+  //   operand2.push(number);
+  //   sNumber = Number(operand2.join(""));
+  // } else if (number === "/") {
+  //   operand2.push(number);
+  //   sNumber = Number(operand2.join(""));
+  // }
+  // console.log(fNumber);
+  // console.log(sNumber);
 }
 
 function display(number) {
-  operations.value += number;
+  operations.value = operations.value + number;
 }
 
 function operands(number) {
@@ -19,12 +46,23 @@ function operands(number) {
   console.log(result);
 }
 function clearAll() {
+  // operations.value = operand1 = [];
   operations.value = "";
   answer.value = "";
+  // operand1 = [];
+  // fNumber = Number(operand1.join(""));
+  // console.log(fNumber);
 }
 
 function deleteLast() {
   operations.value = operations.value.slice(0, -1);
+  // operand1.pop();
+  // fNumber = Number(operand1.join(""));
+  // console.log(fNumber);
+  // // operations.value = operations.value.pop();
+  // // operand1.pop();
+  // // result = Number(operand1.join(""));
+  // // operations.value = result;
 }
 
 function operate(operator) {
@@ -41,4 +79,11 @@ function operate(operator) {
   }
 }
 
-function calculate(operand1) {}
+function negation() {
+  operations.value = operations.value * -1;
+}
+
+function calculate() {
+  expression = operations.value.replace(/\s/g, "");
+  answer.value = helper(Array.from(expression), 0);
+}
