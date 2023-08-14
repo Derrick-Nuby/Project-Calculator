0a2db2ba19b4825e57a5ee37c0e377af6486f4ce
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-08-14 18:10:42 +0200

    calculate button

--- app.js
+++ app.js
@@ -1,43 +1,16 @@
+const one = document.getElementById("one");
 const operations = document.getElementById("operations");
 const answer = document.getElementById("answer");
-let operand1 = [];
-let operand2 = [];
+const operand1 = [];
 let result;
-let fNumber, sNumber;
 
 function allocate(number) {
-  operands(number);
   display(number);
-  // display(number);
-  // operand1.push(number);
-  // fNumber = Number(operand1.join(""));
-  // console.log(fNumber);
-  // if (typeof number === "number") {
-  //   operand1.push(number);
-  //   fNumber = Number(operand1.join(""));
-  //   console.log(fNumber);
-  // } else if (number === "-") {
-  //   operand2.push(number);
-  //   sNumber = Number(operand2.join(""));
-  // } else if (number === "+") {
-  //   operand2.push(number);
-  //   sNumber = Number(operand2.join(""));
-  // } else if (number === "X") {
-  //   operand2.push(number);
-  //   sNumber = Number(operand2.join(""));
-  // } else if (number === "*") {
-  //   operand2.push(number);
-  //   sNumber = Number(operand2.join(""));
-  // } else if (number === "/") {
-  //   operand2.push(number);
-  //   sNumber = Number(operand2.join(""));
-  // }
-  // console.log(fNumber);
-  // console.log(sNumber);
+  operands(number);
 }
 
 function display(number) {
-  operations.value = operations.value + number;
+  operations.value += number;
 }
 
 function operands(number) {
@@ -46,23 +19,12 @@ function operands(number) {
   console.log(result);
 }
 function clearAll() {
-  // operations.value = operand1 = [];
   operations.value = "";
   answer.value = "";
-  // operand1 = [];
-  // fNumber = Number(operand1.join(""));
-  // console.log(fNumber);
 }
 
 function deleteLast() {
   operations.value = operations.value.slice(0, -1);
-  // operand1.pop();
-  // fNumber = Number(operand1.join(""));
-  // console.log(fNumber);
-  // // operations.value = operations.value.pop();
-  // // operand1.pop();
-  // // result = Number(operand1.join(""));
-  // // operations.value = result;
 }
 
 function operate(operator) {
